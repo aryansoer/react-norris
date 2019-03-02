02c7baf0927210a17a9ef5bece78f1c278e2b0d3
Isoev Behruz <bisoev@exadel.com>
2019-03-02 13:36:32 +0300

    Configure preloaded state of the store

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ import './index.css';
 import AppContainer from './containers/AppContainer';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store'
+import { loadState, saveState } from './localStorage';
+import { throttle } from './lib/utils';
 
-const store = configureStore();
+const preloadedState = loadState();
+const store = configureStore(preloadedState);
+
+// Write to the localStorage at most once a second
+store.subscribe(throttle(() => {
+  saveState({
+    app: store.getState().app
+  });
+}, 1000));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppContainer />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
