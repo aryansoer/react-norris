71910bb7d5e519dfd76e9696b55656a066c11ee1
Isoev Behruz <bisoev@exadel.com>
2019-03-03 14:26:29 +0300

    Test utils.random func

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,1 +1,15 @@
-// TODO: Write tests...
+import { replace } from './utils';
+
+describe('replace', () => {
+  it('should change a specified value with a new one', () => {
+    const msg = 'Chuck Norris can\'t test for equality because he has no equal.'
+    const result = replace(msg, 'Chuck Norris', 'Bruce Lee');
+    expect(result).toBe('Bruce Lee can\'t test for equality because he has no equal.');
+  });
+
+  it('should be case-sensitive', () => {
+    const msg = 'Mr Green has a green car.';
+    const result = replace(msg, 'Green', 'Red');
+    expect(result).toBe('Mr Red has a green car.');
+  });
+});
