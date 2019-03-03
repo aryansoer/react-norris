fd7392528e27c3995a04822e23e51081e95ca6d5
Isoev Behruz <bisoev@exadel.com>
2019-03-03 16:35:19 +0300

    Updates

---
+++ precache-manifest.65d51bf6d90abe864204af708b9daf25.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cde2f6b67a62e1ff8a583da745519f3",
+    "url": "/react-norris/static/media/spinner.8cde2f6b.svg"
+  },
+  {
+    "revision": "c952e3bb335100cc57b027262f69812f",
+    "url": "/react-norris/static/media/cog.c952e3bb.svg"
+  },
+  {
+    "revision": "629b03c85cc813439787c3734886c453",
+    "url": "/react-norris/static/media/close.629b03c8.svg"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/react-norris/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c906d3f024569a128821",
+    "url": "/react-norris/static/js/runtime~main.c906d3f0.js"
+  },
+  {
+    "revision": "24162a3a42a97c91242b",
+    "url": "/react-norris/static/js/main.24162a3a.chunk.js"
+  },
+  {
+    "revision": "781aa333f72791e9cf10",
+    "url": "/react-norris/static/js/2.781aa333.chunk.js"
+  },
+  {
+    "revision": "24162a3a42a97c91242b",
+    "url": "/react-norris/static/css/main.b67628c1.chunk.css"
+  },
+  {
+    "revision": "f960ee3072ae401a40a55a39eff89f7c",
+    "url": "/react-norris/index.html"
+  }
+];
